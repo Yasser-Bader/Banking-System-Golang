54cb89d1b93f907c540cbff41e113206c642ca45
Yasser Bader <67002064+Yasser-Bader@users.noreply.github.com>
2025-12-26 16:40:16 +0000

    feat:connect databace

--- test.go
+++ test.go
@@ -4,36 +4,29 @@ import (
 	"fmt"
 	"log"
 
-			// استدعاء الفولدر الجديد
-				// "اسم-الموديول/اسم-الفولدر"
-	"Banking-System-Golang/config" 
-
+	// استدعاء الفولدر الجديد
+	// "اسم-الموديول/اسم-الفولدر"
+	"Banking-System-Golang/config"
+	"Banking-System-Golang/models" 
 	"github.com/joho/godotenv"
-	"gorm.io/gorm"
-	)
+)
 
-							// المودل ممكن تخليه هنا أو تنقله فولدر models بعدين
-							type Product struct {
-								gorm.Model
-									Code  string
-										Price uint
-										}
 
-										func main() {
-											// 1. تحميل ملف .env
-												err := godotenv.Load()
-													if err != nil {
-															log.Fatal("Error loading .env file")
-																}
+func main() {
+	// 1. تحميل ملف .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-																	// 2. استدعاء دالة الاتصال من الفولدر الجديد
-																		config.ConnectDatabase()
+	// 2. استدعاء دالة الاتصال من الفولدر الجديد
+	config.ConnectDatabase()
 
-																			// 3. استخدام الداتابيز
-																				// config.DB لاحظ اننا بنستخدم
-																					config.DB.AutoMigrate(&Product{})
+	// 3. استخدام الداتابيز
+	// config.DB لاحظ اننا بنستخدم
+	config.DB.AutoMigrate(&models.Product{})
 
-																						// تجربة إضافة منتج
-																							config.DB.Create(&Product{Code: "Laptop", Price: 15000})
-																								fmt.Println("تمت العملية بنجاح!")
-																								}
+	// تجربة إضافة منتج
+	config.DB.Create(&models.Product{Code: "Laptop cor i 3", Price: 15000})
+	fmt.Println("تمت العملية بنجاح!")
+}
